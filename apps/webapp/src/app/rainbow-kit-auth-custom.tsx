740ad8ed80fde070324c993d6459074ed34261c7
Vadim <eugene.s@zpoken.ai>
2024-06-06 09:36:02 +0300

    add auth api

--- apps/webapp/src/app/rainbow-kit-auth-custom.tsx
+++ apps/webapp/src/app/rainbow-kit-auth-custom.tsx
@@ -7,6 +7,8 @@ import { Session } from 'next-auth';
 import { signIn, signOut } from 'next-auth/react';
 import React, { useState } from 'react';
 import { SiweMessage } from 'siwe';
+import axiosInstance from '../lib/axios';
+import { ApiRoutes } from '../lib/api-routes';
 
 export const RainbowKitAuthCustomProvider = ({
   children,
@@ -21,7 +23,11 @@ export const RainbowKitAuthCustomProvider = ({
 
   const authenticationAdapter = createAuthenticationAdapter({
     getNonce: async () => {
-      return '3cac1c2025bc0783c0673cee247c6e7ef9a407b030a31e8d1aec44903acf8d1e';
+      const dat = await axiosInstance.get<string>(ApiRoutes.NONCE, {
+        withCredentials: true,
+      });
+
+      return dat.data;
     },
 
     createMessage: ({ nonce, address, chainId }) => {
@@ -44,26 +50,37 @@ export const RainbowKitAuthCustomProvider = ({
       try {
         setStatus('loading');
 
-        const verifyRes = {
-          ok: true,
-          message,
-          signature,
-          address: '0x9a868D58C7F31DAd95626e9632A937Fff69a4F0e',
-          token: 'bbb',
-        };
+        console.log({ message, signature });
+
+        // const verifyRes = {
+        //   ok: true,
+        //   message,
+        //   signature,
+        //   address: '0x9a868D58C7F31DAd95626e9632A937Fff69a4F0e',
+        //   token: 'bbb',
+        // };
+
+        const { data } = await axiosInstance.post(
+          ApiRoutes.VERIFY,
+          {
+            signature,
+            message: JSON.stringify(message),
+          },
+          { withCredentials: true },
+        );
         // const verifyRes = await fetch('/api/verify', {
         //   method: 'POST',
         //   headers: { 'Content-Type': 'application/json' },
         //   body: JSON.stringify({ message, signature }),
         // });
 
-        await signIn('credentials', {
-          address: verifyRes.address,
-          token: verifyRes.token,
-          redirect: false,
-        });
-        setStatus('authenticated');
-        return Boolean(verifyRes.ok);
+        // await signIn('credentials', {
+        //   address: verifyRes.address,
+        //   token: verifyRes.token,
+        //   redirect: false,
+        // });
+        // setStatus('authenticated');
+        // return Boolean(verifyRes.ok);
       } catch (error) {
         setStatus('unauthenticated');
         return false;
