7269f647c99a45e3b45ec1c30d333a1397dc1ef3
Vadim <eugene.s@zpoken.ai>
2024-06-19 15:49:58 +0300

    delete balance from header

--- apps/webapp/src/components/header.tsx
+++ apps/webapp/src/components/header.tsx
@@ -2,37 +2,11 @@
 
 import { BorderWrapper } from '@rearden/ui/components/border-wrapper';
 import { Icons } from '@rearden/ui/components/icons';
-import { getBalance } from '@wagmi/core';
-import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { formatUnits } from 'viem';
-import { wagmiConfig } from '../config/wagmi';
 import { PagePath } from '../constants/nav-routes';
 import { CustomConnectButton } from './custom-connect-button';
 
 export const Header = () => {
-  const { data: session } = useSession();
-
-  const [balance, setBalance] = useState<string>('0.00');
-
-  useEffect(() => {
-    const address = session?.address;
-    if (!address) {
-      setBalance('');
-      return;
-    }
-    void (async () => {
-      const balance = await getBalance(wagmiConfig, {
-        address,
-      });
-
-      const formatted = formatUnits(balance.value, balance.decimals);
-
-      setBalance(formatted);
-    })();
-  }, [session]);
-
   return (
     <BorderWrapper className='px-5 py-4'>
       <Link href={PagePath.INDEX} className='flex items-center gap-2'>
@@ -44,14 +18,6 @@ export const Header = () => {
         <h2>Rearden</h2>
       </Link>
       <div className='flex items-center gap-10'>
-        {session?.address && (
-          <p className='text-base font-bold'>
-            Total balance:{' '}
-            <span className='w-fit bg-primary-gradient bg-clip-text text-base font-bold text-transparent'>
-              ${balance}
-            </span>
-          </p>
-        )}
         <CustomConnectButton />
       </div>
     </BorderWrapper>
